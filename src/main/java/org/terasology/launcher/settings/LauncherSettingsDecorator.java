27ed35355b6781d5a8c9de89af098fd46ddd5c15
Zeeshan Asghar <zeeshan.asghar@devfactory.com>
2016-03-15 14:06:58 +0500

    squid:S1186 Methods should not be empty

--- src/main/java/org/terasology/launcher/settings/LauncherSettingsDecorator.java
+++ src/main/java/org/terasology/launcher/settings/LauncherSettingsDecorator.java
@@ -71,32 +71,32 @@ public class LauncherSettingsDecorator extends LauncherSettings {
 
     @Override
     protected void initInitialHeapSize() {
-
+        throw new UnsupportedOperationException();
     }
 
     @Override
     protected void initSearchForLauncherUpdates() {
-
+        throw new UnsupportedOperationException();
     }
 
     @Override
     protected void initCloseLauncherAfterGameStart() {
-
+        throw new UnsupportedOperationException();
     }
 
     @Override
     protected void initSaveDownloadedFiles() {
-
+        throw new UnsupportedOperationException();
     }
 
     @Override
     protected void initGameDirectory() {
-
+        throw new UnsupportedOperationException();
     }
 
     @Override
     protected void initGameDataDirectory() {
-
+        throw new UnsupportedOperationException();
     }
 
     @Override
@@ -106,37 +106,37 @@ public class LauncherSettingsDecorator extends LauncherSettings {
 
     @Override
     protected void initUserGameParameters() {
-
+        throw new UnsupportedOperationException();
     }
 
     @Override
     protected void initLogLevel() {
-
+        throw new UnsupportedOperationException();
     }
 
     @Override
     protected void initMaxHeapSize() {
-
+        throw new UnsupportedOperationException();
     }
 
     @Override
     protected void initLastBuildNumber() {
-
+        throw new UnsupportedOperationException();
     }
 
     @Override
     protected void initLocale() {
-
+        throw new UnsupportedOperationException();
     }
 
     @Override
     protected void initJob() {
-
+        throw new UnsupportedOperationException();
     }
 
     @Override
     protected void initBuildVersion() {
-
+        throw new UnsupportedOperationException();
     }
 
     // --------------------------------------------------------------------- //
