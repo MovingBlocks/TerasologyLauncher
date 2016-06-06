ca95781f176fc335c8e9364853ecb31641bd7e26
mtjvankuik <mtjvankuik@gmail.com>
2016-06-06 07:26:12 +0200

    import code conventions

--- src/main/java/org/terasology/launcher/util/Languages.java
+++ src/main/java/org/terasology/launcher/util/Languages.java
@@ -19,7 +19,12 @@ package org.terasology.launcher.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.Comparator;
+import java.util.Locale;
+import java.util.List;
+import java.util.TreeMap;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public final class Languages {
 
