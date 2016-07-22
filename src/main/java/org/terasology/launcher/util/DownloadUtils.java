2353d97d61a1c0d45a4e9ec6ed98035350aa27bb
Rasmus Praestholm <cervator@gmail.com>
2016-07-21 20:33:46 -0400

    Fixes #371 by making job result detection less fragile.

--- src/main/java/org/terasology/launcher/util/DownloadUtils.java
+++ src/main/java/org/terasology/launcher/util/DownloadUtils.java
@@ -231,7 +231,7 @@ public final class DownloadUtils {
             final String jsonResult = reader.readLine();
             if (jsonResult != null) {
                 for (JobResult result : JobResult.values()) {
-                    if (jsonResult.indexOf(result.name()) == 11) {
+                    if (jsonResult.contains(result.name())) {
                         jobResult = result;
                         break;
                     }
