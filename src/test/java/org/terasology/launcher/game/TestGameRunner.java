6edab0f0dbe37af9c460349c6ca827bf33b02e40
Max <maximborsch@gmail.com>
2016-12-23 17:44:01 -0500

    GameRunner test

---
+++ src/test/java/org/terasology/launcher/game/TestGameRunner.java
@@ -0,0 +1,145 @@
+package org.terasology.launcher.game;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.internal.util.reflection.Whitebox;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.*;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({GameRunner.class, LoggerFactory.class, Thread.class})
+public class TestGameRunner {
+
+    /**
+     * Passed around to GameRunners to simulate a running game process.
+     */
+    private Process gameProcess;
+
+    @Before
+    public void setup () throws Exception {
+        mockStatic(LoggerFactory.class);
+        mockStatic(Thread.class);
+
+        Logger logger = mock(Logger.class);
+        when(LoggerFactory.getLogger(any(Class.class))).thenReturn(logger);
+
+        // Create fake game process to give to GameRunner
+        gameProcess = mock (Process.class);
+        resetGameProcess();
+    }
+
+    /**
+     * Resets the simulated game process to "successful" behaviour for use in multiple tests.
+     * @throws Exception
+     */
+    private void resetGameProcess () throws Exception {
+        when(gameProcess.getInputStream()).thenReturn(new ByteArrayInputStream(new byte[0]));
+        when(gameProcess.waitFor()).thenReturn(0);
+    }
+
+    @Test
+    public void testGameOutput() throws Exception {
+        resetGameProcess();
+
+        // Simulate game process outputting TEST_STRING
+        String TEST_STRING = "LineOne\nLineTwo";
+        when(gameProcess.getInputStream()).thenReturn(new ByteArrayInputStream(TEST_STRING.getBytes()));
+
+        // Run game process
+        GameRunner gameRunner = new GameRunner(gameProcess);
+        gameRunner.run();
+
+        // Make sure GameRunner logs TEST_STRING line 1
+        verify((Logger) Whitebox.getInternalState(gameRunner, "logger")).trace(
+                "Game output: {}",
+                TEST_STRING.substring(0, TEST_STRING.indexOf("\n"))
+        );
+
+        // Make sure GameRunner logs TEST_STRING line 2
+        verify((Logger) Whitebox.getInternalState(gameRunner, "logger")).trace(
+                "Game output: {}",
+                TEST_STRING.substring(TEST_STRING.indexOf("\n") + 1)
+        );
+    }
+
+    @Test
+    public void testGameExitSuccessful() throws Exception {
+        resetGameProcess();
+
+        // Run game process
+        GameRunner gameRunner = new GameRunner(gameProcess);
+        gameRunner.run();
+
+        // Make sure GameRunner logs that the game exited
+        verify((Logger) Whitebox.getInternalState(gameRunner, "logger"), atLeastOnce()).debug(
+                "Game closed with the exit value '{}'.",
+                0
+        );
+    }
+
+    @Test
+    public void testGameEarlyInterrupt() throws Exception {
+        resetGameProcess();
+
+        GameRunner gameRunner = new GameRunner(gameProcess);
+
+        // Simulate GameRunner running on a thread
+        Thread gameThread = mock(Thread.class);
+        when(Thread.currentThread()).thenReturn(gameThread);
+
+        // Simulate early game thread interruption
+        when(gameThread.isInterrupted()).thenReturn(true);
+
+        // Run game process
+        gameRunner.run();
+
+        // Make sure GameRunner logs that the game thread was interrupted
+        verify((Logger) Whitebox.getInternalState(gameRunner, "logger")).debug("Game thread interrupted.");
+    }
+
+    @Test
+    public void testGameLateInterrupt() throws Exception {
+        resetGameProcess();
+
+        // Simulate late game thread interruption (while game is running)
+        when(gameProcess.waitFor()).thenThrow(new InterruptedException());
+
+        // Run game process
+        GameRunner gameRunner = new GameRunner(gameProcess);
+        gameRunner.run();
+
+        // Make sure GameRunner logs an error with an InterruptedException
+        verify((Logger) Whitebox.getInternalState(gameRunner, "logger")).error(eq("The game thread was interrupted!"), any(InterruptedException.class));
+    }
+
+    @Test
+    public void testGameOutputError() throws Exception {
+        resetGameProcess();
+
+        // Simulate an invalid output stream (that throws an IOException when read from because it's unhappy with its life)
+        InputStream badStream = mock (InputStream.class);
+        when(badStream.read(any(byte[].class), anyInt(), anyInt())).thenThrow(new IOException("Unhappy with life!"));
+
+        when(gameProcess.getInputStream()).thenReturn(badStream);
+
+        // Run game process
+        GameRunner gameRunner = new GameRunner(gameProcess);
+        gameRunner.run();
+
+        // Make sure GameRunner logs an error with an IOException
+        verify((Logger) Whitebox.getInternalState(gameRunner, "logger")).error(eq("Could not read game output!"), any(IOException.class));
+    }
+}
