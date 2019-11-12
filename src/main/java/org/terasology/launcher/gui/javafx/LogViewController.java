2df401c79aa9accc3fc36b374adc1dd8bfad00bd
Tobias Nett <skaldarnar@googlemail.com>
2019-11-12 21:40:40 +0100

    periodically flush the buffered log to the text area to avoid NPEs

--- src/main/java/org/terasology/launcher/gui/javafx/LogViewController.java
+++ src/main/java/org/terasology/launcher/gui/javafx/LogViewController.java
@@ -2,22 +2,47 @@ package org.terasology.launcher.gui.javafx;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.AppenderBase;
+import javafx.concurrent.ScheduledService;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.text.Font;
+import javafx.util.Duration;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 public class LogViewController extends AppenderBase<ILoggingEvent> {
 
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ss.SSS");
 
+    private final StringBuffer buffer;
+
     @FXML
     private TextArea logArea;
+
+    public LogViewController() {
+        buffer = new StringBuffer();
+
+        ScheduledService<Void> schedule = new ScheduledService<Void>() {
+            @Override
+            protected Task<Void> createTask() {
+                return new Task<Void>() {
+                    @Override
+                    protected Void call() throws Exception {
+                        logArea.appendText(buffer.toString());
+                        //TODO figure out whether this is thread-safe (I suspect it's not)
+                        buffer.delete(0, buffer.length());
+                        return null;
+                    }
+                };
+            }
+        };
+        schedule.setPeriod(Duration.seconds(2));
+        schedule.start();
+    }
 
     @FXML
     public void initialize() {
@@ -25,23 +50,20 @@ public class LogViewController extends AppenderBase<ILoggingEvent> {
         logArea.setFont(Font.font("monospaced"));
     }
 
+    private LocalDateTime timestampFromEvent(ILoggingEvent loggingEvent) {
+        return new Date(loggingEvent.getTimeStamp())
+                .toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDateTime();
+    }
+
     @Override
     protected void append(ILoggingEvent loggingEvent) {
+        final String message = loggingEvent.getFormattedMessage();
+        final LocalDateTime timestamp = timestampFromEvent(loggingEvent);
 
-        final LocalDateTime timestamp =
-                new Date(loggingEvent.getTimeStamp())
-                        .toInstant()
-                        .atZone(ZoneId.systemDefault())
-                        .toLocalDateTime();
-
-        try {
-            final String message = loggingEvent.getFormattedMessage();
-
-            logArea.appendText(String.format("%s | %-5s | ", formatter.format(timestamp), loggingEvent.getLevel()));
-            logArea.appendText(message);
-            logArea.appendText("\n");
-        } catch (Exception e) {
-
-        }
+        buffer.append(String.format("%s | %-5s | ", formatter.format(timestamp), loggingEvent.getLevel()));
+        buffer.append(message);
+        buffer.append("\n");
     }
 }
