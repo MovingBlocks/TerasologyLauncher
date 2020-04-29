dee625f233122b09239ce6a0587aad09a18c0bdf
Kevin Turner <83819+keturn@users.noreply.github.com>
2020-04-29 13:27:31 -0700

    maintenance: convert TestGameRunner to using slf4j-test

--- src/test/java/org/terasology/launcher/game/TestGameRunner.java
+++ src/test/java/org/terasology/launcher/game/TestGameRunner.java
@@ -15,44 +15,50 @@
  */
 package org.terasology.launcher.game;
 
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-import org.powermock.reflect.Whitebox;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.powermock.modules.junit4.PowerMockRunnerDelegate;
+import org.spf4j.log.Level;
+import org.spf4j.test.log.LogAssert;
+import org.spf4j.test.log.TestLoggers;
+import org.spf4j.test.log.annotations.CollectLogs;
+import org.spf4j.test.log.annotations.ExpectLog;
+import org.spf4j.test.log.annotations.PrintLogs;
+import org.spf4j.test.log.junit4.Spf4jTestLogJUnitRunner;
+import org.spf4j.test.matchers.LogMatchers;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.anyInt;
-import static org.mockito.Mockito.atLeastOnce;
-import static org.mockito.Mockito.eq;
-import static org.mockito.Mockito.verify;
 import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({GameRunner.class, LoggerFactory.class, Thread.class})
+@PowerMockRunnerDelegate(Spf4jTestLogJUnitRunner.class)
+@PrepareForTest(GameRunner.class)
 public class TestGameRunner {
 
     /**
      * Passed around to GameRunners to simulate a running game process.
      */
     private Process gameProcess;
+    private TestLoggers testLog;
 
     @Before
     public void setup() throws Exception {
-        mockStatic(LoggerFactory.class);
         mockStatic(Thread.class);
 
-        Logger logger = mock(Logger.class);
-        when(LoggerFactory.getLogger(any(Class.class))).thenReturn(logger);
+        testLog = TestLoggers.sys();
 
         // Create fake game process to give to GameRunner
         gameProcess = mock(Process.class);
@@ -70,6 +76,10 @@ public class TestGameRunner {
     }
 
     @Test
+    // CollectLogs(TRACE) isn't necessary for this test's assertions.
+    // Logs are shown on test failure, but the default doesn't include TRACE.
+    // Because TRACE logs are relevant to this test we use this to include them in the output.
+    @CollectLogs(minLevel = Level.TRACE)
     public void testGameOutput() throws Exception {
         resetGameProcess();
 
@@ -77,39 +87,59 @@ public class TestGameRunner {
         String testString = "LineOne\nLineTwo";
         when(gameProcess.getInputStream()).thenReturn(new ByteArrayInputStream(testString.getBytes()));
 
-        // Run game process
+        // Can have checks be relatively loose:
+        // With logs in any category, at TRACE or above,
+        LogAssert broadExpectation = testLog.expect("", Level.TRACE,
+                // expect a message that contains "LineOne"
+                LogMatchers.hasMatchingMessage(containsString("LineOne")),
+                // also a message that contains "LineTwo"
+                LogMatchers.hasMatchingMessage(containsString("LineTwo"))
+                // or to match the message exactly,
+                // LogMatchers.hasMessage("LineTwo")
+        );
+
+        // You can also get very specific about how the log message is made.
+        // We'll define a matcher to look specifically at records whose
+        // format string begin with "Game output"
+        var hasGameOutputFormat = LogMatchers.hasFormatWithPattern("^Game output.*");
+
+        // These expectations are restricted specifically to logs sent to a logger named like this class.
+        LogAssert detailedExpectation = testLog.expect(GameRunner.class.getName(), Level.TRACE,
+                // Check its format against our matcher, and also that it has an argument that is exactly "LineOne"
+                allOf(hasGameOutputFormat, LogMatchers.hasArguments("LineOne")),
+                // ...and also for line two.
+                allOf(hasGameOutputFormat, LogMatchers.hasArguments("LineTwo"))
+        );
+
+        // Log expectations must be set up *before* the logs are generated.
+        // Now we can run the code under test:
         GameRunner gameRunner = new GameRunner(gameProcess);
         gameRunner.run();
 
-        // Make sure GameRunner logs TEST_STRING line 1
-        verify((Logger) Whitebox.getInternalState(GameRunner.class, "logger")).trace(
-                "Game output: {}",
-                testString.substring(0, testString.indexOf("\n"))
-        );
+        // And check to see if the assertions held.
+        broadExpectation.assertObservation();
+        detailedExpectation.assertObservation();
 
-        // Make sure GameRunner logs TEST_STRING line 2
-        verify((Logger) Whitebox.getInternalState(GameRunner.class, "logger")).trace(
-                "Game output: {}",
-                testString.substring(testString.indexOf("\n") + 1)
-        );
+        // be sure to run those assertions! If you set expectations but never check them,
+        // this test runner does *not* notice and warn you.
     }
 
     @Test
-    public void testGameExitSuccessful() throws Exception {
-        resetGameProcess();
-
-        // Run game process
+    // There's an annotation you can use for simple cases. It auto-asserts when the method finishes.
+    @ExpectLog(level = Level.DEBUG, messageRegexp = "Game closed with the exit value '0'.")
+    public void testGameExitSuccessful() {
         GameRunner gameRunner = new GameRunner(gameProcess);
         gameRunner.run();
-
-        // Make sure GameRunner logs that the game exited
-        verify((Logger) Whitebox.getInternalState(GameRunner.class, "logger"), atLeastOnce()).debug(
-                "Game closed with the exit value '{}'.",
-                0
-        );
     }
 
     @Test
+    // The log-printer's default setting is to display all logged errors.
+    // But we know the code under test will be logging an error and that
+    // doesn't mean there's an error in the scenario. We can override the
+    // settings for this logger to not print error logs from that class.
+    //
+    // The error logs will still be collected and displayed if the test fails.
+    @PrintLogs(category = "org.terasology.launcher.game.GameRunner", minLevel = Level.OFF, ideMinLevel = Level.OFF, greedy = true)
     public void testGameEarlyInterrupt() throws Exception {
         resetGameProcess();
 
@@ -122,43 +152,52 @@ public class TestGameRunner {
         // Simulate early game thread interruption
         when(gameThread.isInterrupted()).thenReturn(true);
 
+        var interruptionLogged = testLog.expect("", Level.DEBUG, LogMatchers.hasMessage("Game thread interrupted."));
+
         // Run game process
         gameRunner.run();
 
-        // Make sure GameRunner logs that the game thread was interrupted
-        verify((Logger) Whitebox.getInternalState(GameRunner.class, "logger")).debug("Game thread interrupted.");
+        interruptionLogged.assertObservation();
     }
 
     @Test
+    @PrintLogs(category = "org.terasology.launcher.game.GameRunner", minLevel = Level.OFF, ideMinLevel = Level.OFF, greedy = true)
     public void testGameLateInterrupt() throws Exception {
-        resetGameProcess();
-
         // Simulate late game thread interruption (while game is running)
         when(gameProcess.waitFor()).thenThrow(new InterruptedException());
+
+        var loggedException = testLog.expect("", Level.ERROR, Matchers.allOf(
+                LogMatchers.hasMatchingExtraThrowable(Matchers.instanceOf(InterruptedException.class)),
+                LogMatchers.hasMessage("The game thread was interrupted!")
+        ));
 
         // Run game process
         GameRunner gameRunner = new GameRunner(gameProcess);
         gameRunner.run();
 
         // Make sure GameRunner logs an error with an InterruptedException
-        verify((Logger) Whitebox.getInternalState(GameRunner.class, "logger")).error(eq("The game thread was interrupted!"), any(InterruptedException.class));
+        loggedException.assertObservation();
     }
 
     @Test
+    @PrintLogs(category = "org.terasology.launcher.game.GameRunner", minLevel = Level.OFF, ideMinLevel = Level.OFF, greedy = true)
     public void testGameOutputError() throws Exception {
-        resetGameProcess();
-
         // Simulate an invalid output stream (that throws an IOException when read from because it's unhappy with its life)
         InputStream badStream = mock(InputStream.class);
         when(badStream.read(any(byte[].class), anyInt(), anyInt())).thenThrow(new IOException("Unhappy with life!"));
 
         when(gameProcess.getInputStream()).thenReturn(badStream);
 
+        var loggedException = testLog.expect("", Level.ERROR, Matchers.allOf(
+                LogMatchers.hasMatchingExtraThrowable(Matchers.instanceOf(IOException.class)),
+                LogMatchers.hasMessage("Could not read game output!")
+        ));
+
         // Run game process
         GameRunner gameRunner = new GameRunner(gameProcess);
         gameRunner.run();
 
         // Make sure GameRunner logs an error with an IOException
-        verify((Logger) Whitebox.getInternalState(GameRunner.class, "logger")).error(eq("Could not read game output!"), any(IOException.class));
+        loggedException.assertObservation();
     }
 }
