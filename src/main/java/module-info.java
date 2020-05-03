58e562805a6f5fc994eeabea074e6e592446cba3
Tobias Nett <skaldarnar@googlemail.com>
2020-05-03 12:59:11 +0200

    WIP: Attempt to provide `module-info.java`

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module org.terasology.launcher {
+    // JavaFX
+    requires javafx.controls;
+
+    // Logging
+    requires org.slf4j;
+
+    // Other modules
+    requires gson;
+    requires com.google.common;
+}
