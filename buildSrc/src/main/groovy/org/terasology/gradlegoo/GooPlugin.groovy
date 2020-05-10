6a9305643c9da649ea6789800e252653b63eef3a
Kevin Turner <83819+keturn@users.noreply.github.com>
2020-05-10 11:37:25 -0700

    GradleGoo.prefers: provide Action<ResolutionStrategy> for a more gradle-like interface

--- buildSrc/src/main/groovy/org/terasology/gradlegoo/GooPlugin.groovy
+++ buildSrc/src/main/groovy/org/terasology/gradlegoo/GooPlugin.groovy
@@ -1,12 +1,12 @@
 package org.terasology.gradlegoo
 
+import org.gradle.api.Action
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.ComponentMetadataContext
 import org.gradle.api.artifacts.ComponentMetadataRule
 import org.gradle.api.artifacts.ResolutionStrategy
 import org.gradle.api.artifacts.component.ModuleComponentIdentifier
-
 
 @SuppressWarnings("unused")
 class GradleGooPlugin implements Plugin<Project> {
@@ -40,19 +40,23 @@ class GradleGooExtension {
      * This is just the code from here:
      * https://docs.gradle.org/current/userguide/dependency_capability_conflict.html#sub:selecting-between-candidates
      *
-     * They knew conflicts would happen, that's their motivation for the feature. They even use this *exact thing* in their examples, slf4j providers. And clearly, once
-     * you have conflicts, you'll want a way to specify their resolution. The manual talks about it at great length.
+     * They knew conflicts would happen, that's their motivation for the feature. They even use this *exact thing* in
+     * their examples, slf4j bindings. And clearly, once you have conflicts, you'll want a way to specify their
+     * resolution. The manual talks about it at great length.
      *
      * So why didn't they make the common-cases a one-liner instead of ten lines to copy and paste?
      *
-     * @param strategy probably `getResolutionStrategy()`
      * @param group the capability's group name, e.g. `logging`
      * @param capability the capability name, e.g. `slf4j-impl-capability`
      * @param preferredModule the name of the module you want to win, e.g. `logback-classic`
      * @param reason shown in debug messages about conflict resolution
      */
-    @SuppressWarnings("unused")
-    static Void prefers(ResolutionStrategy strategy, String group, String capability, String preferredModule, String reason) {
+    static Action<ResolutionStrategy> prefers(String group, String capability, String preferredModule, String reason) {
+        return { ResolutionStrategy strategy -> prefers(strategy, group, capability, preferredModule, reason) };
+    }
+
+    static void prefers(ResolutionStrategy strategy, String group, String capability, String preferredModule,
+                        String reason) {
         strategy.capabilitiesResolution.withCapability(group, capability) {
             def toBeSelected = candidates.find {
                 it.id instanceof ModuleComponentIdentifier && it.id.module == preferredModule
@@ -60,9 +64,7 @@ class GradleGooExtension {
             if (toBeSelected != null) {
                 select(toBeSelected)
             }
-            println("For ${capability} Chose ${toBeSelected} over ${candidates}")
             because reason
         }
-        return null
     }
 }
