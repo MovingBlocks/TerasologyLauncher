6b002a5cb8e0c01f92e6b36a665b60d6976dd706
Kevin Turner <83819+keturn@users.noreply.github.com>
2020-05-10 16:53:20 -0700

    FxTimer: apply local style convention, remove unused methods

--- src/main/java/org/terasology/launcher/gui/javafx/FXUtils.java
+++ src/main/java/org/terasology/launcher/gui/javafx/FXUtils.java
@@ -16,14 +16,13 @@
 
 package org.terasology.launcher.gui.javafx;
 
-import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.ScaleTransition;
 import javafx.animation.Timeline;
 import javafx.scene.Node;
 import javafx.util.Duration;
 
-final class FXUtils {
+public final class FXUtils {
 
     private FXUtils() { }
 
@@ -31,7 +30,7 @@ final class FXUtils {
      * Creates a {@link javafx.animation.ScaleTransition} with the given factor for the specified node element.
      *
      * @param factor the scaling factor
-     * @param node   the target node
+     * @param node the target node
      * @return a transition object
      */
     static ScaleTransition createScaleTransition(final double factor, final Node node) {
@@ -44,13 +43,33 @@ final class FXUtils {
     }
 
     /**
-     * Provides factory methods for timers that are manipulated from and execute
-     * their action on the JavaFX application thread.
+     * Provides factory methods for timers that are manipulated from and execute their action on the JavaFX application
+     * thread.
      *
-     * Adopted from https://github.com/TomasMikula/ReactFX/blob/v2.0-M5/reactfx/src/main/java/org/reactfx/util/FxTimer.java
-     * by BSD license.
+     * Derived from ReactFX.
+     *
+     * @see <a href="https://github.com/TomasMikula/ReactFX/blob/v2.0-M5/reactfx/src/main/java/org/reactfx/util/FxTimer.java">ReactFX</a>
      */
-    public static class FxTimer {
+    public static final class FxTimer {
+        private final Duration actionTime;
+        private final Timeline timeline;
+        private final Runnable action;
+
+        private long seq;
+
+
+        private FxTimer(java.time.Duration actionTime, java.time.Duration period, Runnable action, int cycles) {
+            this.actionTime = Duration.millis(actionTime.toMillis());
+            this.timeline = new Timeline();
+            this.action = action;
+
+            timeline.getKeyFrames().add(new KeyFrame(this.actionTime)); // used as placeholder
+            if (period != actionTime) {
+                timeline.getKeyFrames().add(new KeyFrame(Duration.millis(period.toMillis())));
+            }
+
+            timeline.setCycleCount(cycles);
+        }
 
         /**
          * Prepares a (stopped) timer that lasts for {@code delay} and whose action runs when timer <em>ends</em>.
@@ -68,64 +87,11 @@ final class FXUtils {
             return timer;
         }
 
-        /**
-         * Prepares a (stopped) timer that lasts for {@code interval} and that executes the given action periodically
-         * when the timer <em>ends</em>.
-         */
-        public static FxTimer createPeriodic(java.time.Duration interval, Runnable action) {
-            return new FxTimer(interval, interval, action, Animation.INDEFINITE);
-        }
-
-        /**
-         * Equivalent to {@code createPeriodic(interval, action).restart()}.
-         */
-        public static FxTimer runPeriodically(java.time.Duration interval, Runnable action) {
-            FxTimer timer = createPeriodic(interval, action);
-            timer.restart();
-            return timer;
-        }
-
-        /**
-         * Prepares a (stopped) timer that lasts for {@code interval} and that executes the given action periodically
-         * when the timer <em>starts</em>.
-         */
-        public static FxTimer createPeriodic0(java.time.Duration interval, Runnable action) {
-            return new FxTimer(java.time.Duration.ZERO, interval, action, Animation.INDEFINITE);
-        }
-
-        /**
-         * Equivalent to {@code createPeriodic0(interval, action).restart()}.
-         */
-        public static FxTimer runPeriodically0(java.time.Duration interval, Runnable action) {
-            FxTimer timer = createPeriodic0(interval, action);
-            timer.restart();
-            return timer;
-        }
-
-        private final Duration actionTime;
-        private final Timeline timeline;
-        private final Runnable action;
-
-        private long seq = 0;
-
-        private FxTimer(java.time.Duration actionTime, java.time.Duration period, Runnable action, int cycles) {
-            this.actionTime = Duration.millis(actionTime.toMillis());
-            this.timeline = new Timeline();
-            this.action = action;
-
-            timeline.getKeyFrames().add(new KeyFrame(this.actionTime)); // used as placeholder
-            if (period != actionTime) {
-                timeline.getKeyFrames().add(new KeyFrame(Duration.millis(period.toMillis())));
-            }
-
-            timeline.setCycleCount(cycles);
-        }
-
         public void restart() {
             stop();
             long expected = seq;
             timeline.getKeyFrames().set(0, new KeyFrame(actionTime, ae -> {
-                if(seq == expected) {
+                if (seq == expected) {
                     action.run();
                 }
             }));
