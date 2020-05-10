ce704fa2e9803fc96c5b5085900f2ee6762deb3f
Kevin Turner <83819+keturn@users.noreply.github.com>
2020-05-10 16:26:50 -0700

    Adopt FxTimer from ReactFx
    https://github.com/TomasMikula/ReactFX/blob/v2.0-M5/reactfx/src/main/java/org/reactfx/util/FxTimer.java
    
    Copyright (c) 2013-2014, Tomas Mikula
    licensed under http://opensource.org/licenses/BSD-2-Clause

--- src/main/java/org/terasology/launcher/gui/javafx/FXUtils.java
+++ src/main/java/org/terasology/launcher/gui/javafx/FXUtils.java
@@ -16,7 +16,10 @@
 
 package org.terasology.launcher.gui.javafx;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
 import javafx.animation.ScaleTransition;
+import javafx.animation.Timeline;
 import javafx.scene.Node;
 import javafx.util.Duration;
 
@@ -40,4 +43,98 @@ final class FXUtils {
         return scaleTransition;
     }
 
+    /**
+     * Provides factory methods for timers that are manipulated from and execute
+     * their action on the JavaFX application thread.
+     *
+     * Adopted from https://github.com/TomasMikula/ReactFX/blob/v2.0-M5/reactfx/src/main/java/org/reactfx/util/FxTimer.java
+     * by BSD license.
+     */
+    public static class FxTimer {
+
+        /**
+         * Prepares a (stopped) timer that lasts for {@code delay} and whose action runs when timer <em>ends</em>.
+         */
+        public static FxTimer create(java.time.Duration delay, Runnable action) {
+            return new FxTimer(delay, delay, action, 1);
+        }
+
+        /**
+         * Equivalent to {@code create(delay, action).restart()}.
+         */
+        public static FxTimer runLater(java.time.Duration delay, Runnable action) {
+            FxTimer timer = create(delay, action);
+            timer.restart();
+            return timer;
+        }
+
+        /**
+         * Prepares a (stopped) timer that lasts for {@code interval} and that executes the given action periodically
+         * when the timer <em>ends</em>.
+         */
+        public static FxTimer createPeriodic(java.time.Duration interval, Runnable action) {
+            return new FxTimer(interval, interval, action, Animation.INDEFINITE);
+        }
+
+        /**
+         * Equivalent to {@code createPeriodic(interval, action).restart()}.
+         */
+        public static FxTimer runPeriodically(java.time.Duration interval, Runnable action) {
+            FxTimer timer = createPeriodic(interval, action);
+            timer.restart();
+            return timer;
+        }
+
+        /**
+         * Prepares a (stopped) timer that lasts for {@code interval} and that executes the given action periodically
+         * when the timer <em>starts</em>.
+         */
+        public static FxTimer createPeriodic0(java.time.Duration interval, Runnable action) {
+            return new FxTimer(java.time.Duration.ZERO, interval, action, Animation.INDEFINITE);
+        }
+
+        /**
+         * Equivalent to {@code createPeriodic0(interval, action).restart()}.
+         */
+        public static FxTimer runPeriodically0(java.time.Duration interval, Runnable action) {
+            FxTimer timer = createPeriodic0(interval, action);
+            timer.restart();
+            return timer;
+        }
+
+        private final Duration actionTime;
+        private final Timeline timeline;
+        private final Runnable action;
+
+        private long seq = 0;
+
+        private FxTimer(java.time.Duration actionTime, java.time.Duration period, Runnable action, int cycles) {
+            this.actionTime = Duration.millis(actionTime.toMillis());
+            this.timeline = new Timeline();
+            this.action = action;
+
+            timeline.getKeyFrames().add(new KeyFrame(this.actionTime)); // used as placeholder
+            if (period != actionTime) {
+                timeline.getKeyFrames().add(new KeyFrame(Duration.millis(period.toMillis())));
+            }
+
+            timeline.setCycleCount(cycles);
+        }
+
+        public void restart() {
+            stop();
+            long expected = seq;
+            timeline.getKeyFrames().set(0, new KeyFrame(actionTime, ae -> {
+                if(seq == expected) {
+                    action.run();
+                }
+            }));
+            timeline.play();
+        }
+
+        public void stop() {
+            timeline.stop();
+            ++seq;
+        }
+    }
 }
