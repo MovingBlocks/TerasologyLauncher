f2267df9d86abd781b0967fe88a4c0c3613b1486
Kevin Turner <83819+keturn@users.noreply.github.com>
2020-05-19 15:11:46 -0700

    doc(test): document Matcher.hasItemsFrom
    copied from upstream

--- src/test/java/org/terasology/launcher/Matchers.java
+++ src/test/java/org/terasology/launcher/Matchers.java
@@ -27,6 +27,21 @@ public final class Matchers {
 
     private Matchers() { }
 
+    /**
+     * Creates a matcher for {@link Iterable}s that matches when consecutive passes over the
+     * examined {@link Iterable} yield at least one item that is equal to the corresponding
+     * item from the specified <code>items</code>.  Whilst matching, each traversal of the
+     * examined {@link Iterable} will stop as soon as a matching item is found.
+     * <p>
+     * For example:
+     * <p>
+     * {@code
+     * assertThat(Arrays.asList("foo", "bar", "baz"), hasItemsFrom(List.of("baz", "foo")))
+     * }
+     *
+     * @param items
+     *     the items to compare against the items provided by the examined {@link Iterable}
+     */
     public static <T> Matcher<Iterable<? super T>> hasItemsFrom(Collection<T> items) {
         /* org.hamcrest.Matchers.hasItems(T...) takes variable arguments, so if
          * we want to match against a list, we reimplement it.
