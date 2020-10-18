cd24adb95db64a3e6cc374725d6b8789671ec2de
Malanius Privierre <malaniusprivierre@gmail.com>
2020-10-18 12:40:22 +0200

    Prevent IntelliJ from making * imports

--- src/test/java/org/terasology/launcher/util/TestFileUtils.java
+++ src/test/java/org/terasology/launcher/util/TestFileUtils.java
@@ -41,7 +41,11 @@ import java.util.zip.ZipOutputStream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.startsWith;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestFileUtils {
 
