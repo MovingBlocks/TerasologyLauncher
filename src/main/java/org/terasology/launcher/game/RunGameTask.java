981ad538cd18ace133492eb6fcf532d722600290
Kevin Turner <83819+keturn@users.noreply.github.com>
2020-10-18 20:09:40 -0700

    fix (RunGameTask): make sure we don't leave a Reader hanging open.

--- src/main/java/org/terasology/launcher/game/RunGameTask.java
+++ src/main/java/org/terasology/launcher/game/RunGameTask.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2020 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 
 package org.terasology.launcher.game;
 
@@ -24,6 +11,7 @@ import org.slf4j.event.Level;
 import org.terasology.launcher.gui.javafx.FxTimer;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.time.Duration;
 import java.util.concurrent.Callable;
@@ -127,8 +115,11 @@ class RunGameTask extends Task<Boolean> {
         startTimer();
 
         // log each line of process output
-        var gameOutput = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        gameOutput.lines().forEachOrdered(this::handleOutputLine);
+        try (var gameOutput = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            gameOutput.lines().forEachOrdered(this::handleOutputLine);
+        } catch (IOException e) {
+            logger.error("Exception reading process output.", e);
+        }
 
         try {
             // The output has closed, so we _often_ have the exit value immediately, but apparently
