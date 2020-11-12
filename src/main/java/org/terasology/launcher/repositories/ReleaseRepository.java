0151b95f66d7a535a0b0467fd5343d78ef616c9b
Tobias Nett <skaldarnar@googlemail.com>
2020-11-12 20:28:55 +0100

    connect to old Jenkins to fetch releases (package `repositories`)
    I'm free to suggestions for a better name for this package. This is mainly the old code of the JenkinsRepository slightly massaged to the new API. Internally, not much has changed.

---
+++ src/main/java/org/terasology/launcher/repositories/ReleaseRepository.java
@@ -0,0 +1,29 @@
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+package org.terasology.launcher.repositories;
+
+import org.terasology.launcher.model.GameRelease;
+
+import java.util.List;
+
+/**
+ * Common interface for sources of game releaes.
+ *
+ * <p>
+ * Each release repository provides a list of game releases. This is a common API for potentially different sources,
+ * such as GitHub, Jenkins build servers, or other similar. A new release repository has to be added in-code by
+ * implementing this interface and registering it in the {@link RepositoryManager}.
+ * </p>
+ */
+public interface ReleaseRepository {
+
+    /**
+     * Retrieve the list of available game releases from this release repository.
+     *
+     * @return a list of available game releases (an empty list if fetching was not successful)
+     */
+    //TODO: this should probably throw an IOException in case of connection erros so that the UI can decide whether to
+    //      notify the user about that.
+    List<GameRelease> fetchReleases();
+}
