efe5153f043c89c61b81afc0472902e0ebd51c5d
Tobias Nett <skaldarnar@googlemail.com>
2021-02-11 10:53:46 +0100

    test(repositories): add more tests for happy/happier paths

--- src/test/java/org/terasology/launcher/repositories/JenkinsRepositoryAdapterTest.java
+++ src/test/java/org/terasology/launcher/repositories/JenkinsRepositoryAdapterTest.java
@@ -4,13 +4,16 @@ import static org.mockito.Mockito.when;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Optional;
 
+import com.google.common.collect.Lists;
 import com.google.gson.Gson;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.terasology.launcher.model.Build;
+import org.terasology.launcher.model.GameRelease;
 import org.terasology.launcher.model.Profile;
 
 public class JenkinsRepositoryAdapterTest {
@@ -49,5 +52,43 @@ public class JenkinsRepositoryAdapterTest {
     Mockito.when(stub.openConnection()).thenReturn(stubReader);
 
     Assertions.assertTrue(adapter.fetchReleases().isEmpty());  
+  }
+
+  @Test
+  void fetchReleases_assumeInvalidRelease() throws IOException {
+    Jenkins.Build buildStub = new Jenkins.Build();
+    Jenkins.ApiResult resultStub = new Jenkins.ApiResult();
+    resultStub.builds = new Jenkins.Build[]{buildStub};
+
+    Gson gsonStub = Mockito.mock(Gson.class);
+    Mockito.doReturn(resultStub).when(gsonStub).fromJson(Mockito.any(BufferedReader.class), Mockito.eq(Jenkins.ApiResult.class));
+
+    final JenkinsRepositoryAdapter adapter = new JenkinsRepositoryAdapter(Profile.OMEGA, Build.STABLE, gsonStub);
+    final JenkinsRepositoryAdapter spy = Mockito.spy(adapter);
+
+    Mockito.doReturn(Optional.empty()).when(spy).computeReleaseFrom(buildStub);
+
+    Assertions.assertTrue(spy.fetchReleases().isEmpty());
+    // behavior to ensure that we are testing the correct code path
+    Mockito.verify(spy).computeReleaseFrom(buildStub);
+  }
+
+  @Test
+  void fetchReleases_assumeValidRelease() throws IOException {
+    final GameRelease expected = new GameRelease(null, null, null, null);
+
+    Jenkins.Build buildStub = new Jenkins.Build();
+    Jenkins.ApiResult resultStub = new Jenkins.ApiResult();
+    resultStub.builds = new Jenkins.Build[]{buildStub};
+
+    Gson gsonStub = Mockito.mock(Gson.class);
+    Mockito.doReturn(resultStub).when(gsonStub).fromJson(Mockito.any(BufferedReader.class), Mockito.eq(Jenkins.ApiResult.class));
+
+    final JenkinsRepositoryAdapter adapter = new JenkinsRepositoryAdapter(Profile.OMEGA, Build.STABLE, gsonStub);
+    final JenkinsRepositoryAdapter spy = Mockito.spy(adapter);
+
+    Mockito.doReturn(Optional.of(expected)).when(spy).computeReleaseFrom(buildStub);
+
+    Assertions.assertIterableEquals(Lists.newArrayList(expected), spy.fetchReleases());
   }
 }
