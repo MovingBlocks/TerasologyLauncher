8c08576e64e48c4b3e03df75d3baa8a78eb9a8b8
Tobias Nett <skaldarnar@googlemail.com>
2021-02-16 15:37:25 +0100

    refactor(model): use ReleaseMetadata in GameRelease

--- src/main/java/org/terasology/launcher/repositories/LegacyJenkinsRepositoryAdapter.java
+++ src/main/java/org/terasology/launcher/repositories/LegacyJenkinsRepositoryAdapter.java
@@ -9,6 +9,7 @@ import org.terasology.launcher.model.Build;
 import org.terasology.launcher.model.GameIdentifier;
 import org.terasology.launcher.model.GameRelease;
 import org.terasology.launcher.model.Profile;
+import org.terasology.launcher.model.ReleaseMetadata;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -65,6 +66,41 @@ class LegacyJenkinsRepositoryAdapter implements ReleaseRepository {
                 .orElse(new ArrayList<>());
     }
 
+    /**
+     * The PR upgrading the engine to LWJGL v3 (https://github.com/MovingBlocks/Terasology/pull/3969) was merged on
+     * Oct 24, 2020, 20:12 UTC as commit 'c83655fb94c02d68fb8ba31fdb1954e81dde12d6'.
+     * <p>
+     * This method does some reverse engineering of which build on which Jenkins job contains this change. This should
+     * probably be baked into the {@link GameIdentifier} while fetching the info from the remote source. With upcoming
+     * refactoring I'd like to keep this separate here for now...
+     *
+     * @param buildNumber the Jenkins build numbers (from jenkins.terasology.org)
+     * @return
+     */
+    private boolean isLwjgl3(int buildNumber) {
+        if (profile.equals(Profile.OMEGA) && buildProfile.equals(Build.STABLE)) {
+            return buildNumber > 37;
+        }
+        if (profile.equals(Profile.OMEGA) && buildProfile.equals(Build.NIGHTLY)) {
+            return buildNumber > 1103;
+        }
+        if (profile.equals(Profile.ENGINE) && buildProfile.equals(Build.STABLE)) {
+            return buildNumber > 82;
+        }
+        if (profile.equals(Profile.ENGINE) && buildProfile.equals(Build.NIGHTLY)) {
+            return buildNumber > 2317;
+        }
+        return false;
+    }
+
+    private ReleaseMetadata computeReleaseMetadataFrom(Jenkins.Build jenkinsBuildInfo) {
+        final List<String> changelog = computeChangelogFrom(jenkinsBuildInfo.changeSet);
+        final Date timestamp = new Date(jenkinsBuildInfo.timestamp);
+        final boolean isLwjgl3 = isLwjgl3(Integer.parseInt(jenkinsBuildInfo.number));
+        // all builds from this Jenkins are using LWJGL v3
+        return new ReleaseMetadata(changelog, timestamp, isLwjgl3);
+    }
+
     public List<GameRelease> fetchReleases() {
         final List<GameRelease> pkgList = new LinkedList<>();
         final String apiUrl = baseUrl + "job/" + jobName + "/" + API_FILTER;
@@ -76,12 +112,11 @@ class LegacyJenkinsRepositoryAdapter implements ReleaseRepository {
             if (result != null && result.builds != null) {
                 for (Jenkins.Build build : result.builds) {
                     if (hasAcceptableResult(build)) {
-                        final List<String> changelog = computeChangelogFrom(build.changeSet);
                         final URL url = client.getArtifactUrl(build, TERASOLOGY_ZIP_PATTERN);
                         if (url != null) {
                             final GameIdentifier id = new GameIdentifier(build.number, buildProfile, profile);
-                            final Date timestamp = new Date(build.timestamp);
-                            final GameRelease release = new GameRelease(id, url, changelog, timestamp);
+                            final ReleaseMetadata releaseMetadata = computeReleaseMetadataFrom(build);
+                            final GameRelease release = new GameRelease(id, url, releaseMetadata);
                             pkgList.add(release);
                         } else {
                             logger.debug("Skipping build without game artifact: '{}'", build.url);
