6b58d14d698318a2f3d4d51979580f136d7a5c2b
Tobias Nett <skaldarnar@googlemail.com>
2021-02-17 11:29:25 +0100

    test(JenkinsClient): test that invalid JSON payload is handled gracefully (FAILING)

--- src/test/java/org/terasology/launcher/repositories/JenkinsClientTest.java
+++ src/test/java/org/terasology/launcher/repositories/JenkinsClientTest.java
@@ -7,13 +7,21 @@ import com.google.gson.Gson;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 
+/**
+ * Note: We are testing with the default Gson parser here. This can go out of sync with the instantiation of
+ * JenkinsClient in RepositoryManager. Using common dependency injection for productive code and tests would ensure we
+ * are using the "real thing" under test here.
+ */
 @DisplayName("JenkinsClient")
 class JenkinsClientTest {
 
@@ -27,5 +35,19 @@ class JenkinsClientTest {
         doThrow(IOException.class).when(urlThrowingException).openStream();
 
         assertNull(client.request(urlThrowingException));
+    }
+
+    @Test
+    @DisplayName("can handle invalid JSON payload")
+    void canHandleInvalidJsonPayload() throws IOException {
+        final Gson gson = new Gson();
+        final JenkinsClient client = new JenkinsClient(gson);
+
+        InputStream invalidPayload = new ByteArrayInputStream("{ this is ] no json |[!".getBytes());
+
+        URL urlToInvalidPayload = mock(URL.class);
+        doReturn(invalidPayload).when(urlToInvalidPayload).openStream();
+
+        assertNull(client.request(urlToInvalidPayload));
     }
 }
