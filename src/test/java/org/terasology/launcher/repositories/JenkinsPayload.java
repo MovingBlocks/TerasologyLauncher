0ac6a844661a19934e60aad47882acd12ae0e3ae
Tobias Nett <skaldarnar@googlemail.com>
2021-02-21 11:34:07 +0100

    chore: sort test payloads into V1 and V2 (addresses checkstyle warnings) (#632)

--- src/test/java/org/terasology/launcher/repositories/JenkinsPayload.java
+++ src/test/java/org/terasology/launcher/repositories/JenkinsPayload.java
@@ -5,7 +5,7 @@ package org.terasology.launcher.repositories;
 
 import java.util.List;
 
-class JenkinsPayload {
+final class JenkinsPayload {
     private JenkinsPayload() {
 
     }
@@ -127,85 +127,84 @@ class JenkinsPayload {
      */
     static class V2 {
 
-    }
+        static String validPayload() {
+            return "{\n" +
+                    "  \"builds\": [\n" +
+                    "    {\n" +
+                    "      \"artifacts\": [\n" +
+                    "        {\n" +
+                    "          \"fileName\": \"TerasologyOmega.zip\",\n" +
+                    "          \"relativePath\": \"distros/omega/build/distributions/TerasologyOmega.zip\"\n" +
+                    "        },\n" +
+                    "        {\n" +
+                    "          \"fileName\": \"versionInfo.properties\",\n" +
+                    "          \"relativePath\": \"distros/omega/versionInfo.properties\"\n" +
+                    "        }\n" +
+                    "      ],\n" +
+                    "      \"number\": 1,\n" +
+                    "      \"result\": \"SUCCESS\",\n" +
+                    "      \"timestamp\": 1604285977306,\n" +
+                    "      \"url\": \"http://jenkins.terasology.io/teraorg/job/Nanoware/job/Omega/job/develop/1/\"\n" +
+                    "    }\n" +
+                    "  ]\n" +
+                    "}";
+        }
 
-    static String validPayload() {
-        return "{\n" +
-                "  \"builds\": [\n" +
-                "    {\n" +
-                "      \"artifacts\": [\n" +
-                "        {\n" +
-                "          \"fileName\": \"TerasologyOmega.zip\",\n" +
-                "          \"relativePath\": \"distros/omega/build/distributions/TerasologyOmega.zip\"\n" +
-                "        },\n" +
-                "        {\n" +
-                "          \"fileName\": \"versionInfo.properties\",\n" +
-                "          \"relativePath\": \"distros/omega/versionInfo.properties\"\n" +
-                "        }\n" +
-                "      ],\n" +
-                "      \"number\": 1,\n" +
-                "      \"result\": \"SUCCESS\",\n" +
-                "      \"timestamp\": 1604285977306,\n" +
-                "      \"url\": \"http://jenkins.terasology.io/teraorg/job/Nanoware/job/Omega/job/develop/1/\"\n" +
-                "    }\n" +
-                "  ]\n" +
-                "}";
-    }
+        static String nullArtifactsPayload() {
+            return "{ \n" +
+                    "  \"builds\": [\n" +
+                    "    {\n" +
+                    "      \"number\": 1, \"result\": \"SUCCESS\", \"timestamp\": 1604285977306, \n" +
+                    "      \"url\": \"http://jenkins.terasology.io/teraorg/job/Nanoware/job/Omega/job/develop/1/\"\n" +
+                    "    }\n" +
+                    "  ]\n" +
+                    "}";
+        }
 
-    static String nullArtifactsPayload() {
-        return "{ \n" +
-                "  \"builds\": [\n" +
-                "    {\n" +
-                "      \"number\": 1, \"result\": \"SUCCESS\", \"timestamp\": 1604285977306, \n" +
-                "      \"url\": \"http://jenkins.terasology.io/teraorg/job/Nanoware/job/Omega/job/develop/1/\"\n" +
-                "    }\n" +
-                "  ]\n" +
-                "}";
-    }
+        static String emptyArtifactsPayload() {
+            return "{\n" +
+                    "  \"builds\": [\n" +
+                    "    {\n" +
+                    "      \"artifacts\": [],\n" +
+                    "      \"number\": 1,\n" +
+                    "      \"result\": \"SUCCESS\",\n" +
+                    "      \"timestamp\": 1604285977306,\n" +
+                    "      \"url\": \"http://jenkins.terasology.io/teraorg/job/Nanoware/job/Omega/job/develop/1/\"\n" +
+                    "    }\n" +
+                    "  ]\n" +
+                    "}";
+        }
 
-    static String emptyArtifactsPayload() {
-        return "{\n" +
-                "  \"builds\": [\n" +
-                "    {\n" +
-                "      \"artifacts\": [],\n" +
-                "      \"number\": 1,\n" +
-                "      \"result\": \"SUCCESS\",\n" +
-                "      \"timestamp\": 1604285977306,\n" +
-                "      \"url\": \"http://jenkins.terasology.io/teraorg/job/Nanoware/job/Omega/job/develop/1/\"\n" +
-                "    }\n" +
-                "  ]\n" +
-                "}";
-    }
+        /**
+         * Both artifacts {@code versionInfo.properties} and {@code TerasologyOmega.zip} are required, this is missing one of them.
+         */
+        static String incompleteArtifactsPayload() {
+            return "{\n" +
+                    "  \"builds\": [\n" +
+                    "    {\n" +
+                    "      \"artifacts\": [\n" +
+                    "        {\n" +
+                    "          \"fileName\": \"versionInfo.properties\",\n" +
+                    "          \"relativePath\": \"distros/omega/versionInfo.properties\"\n" +
+                    "        }\n" +
+                    "      ],\n" +
+                    "      \"number\": 1,\n" +
+                    "      \"result\": \"SUCCESS\",\n" +
+                    "      \"timestamp\": 1604285977306,\n" +
+                    "      \"url\": \"http://jenkins.terasology.io/teraorg/job/Nanoware/job/Omega/job/develop/1/\"\n" +
+                    "    }\n" +
+                    "  ]\n" +
+                    "}";
+        }
 
-    /**
-     * Both artifacts {@code versionInfo.properties} and {@code TerasologyOmega.zip} are required, this is missing one of them.
-     */
-    static String incompleteArtifactsPayload() {
-        return "{\n" +
-                "  \"builds\": [\n" +
-                "    {\n" +
-                "      \"artifacts\": [\n" +
-                "        {\n" +
-                "          \"fileName\": \"versionInfo.properties\",\n" +
-                "          \"relativePath\": \"distros/omega/versionInfo.properties\"\n" +
-                "        }\n" +
-                "      ],\n" +
-                "      \"number\": 1,\n" +
-                "      \"result\": \"SUCCESS\",\n" +
-                "      \"timestamp\": 1604285977306,\n" +
-                "      \"url\": \"http://jenkins.terasology.io/teraorg/job/Nanoware/job/Omega/job/develop/1/\"\n" +
-                "    }\n" +
-                "  ]\n" +
-                "}";
-    }
-
-    static List<String> incompletePayloads() {
-        return List.of(
-                "{}",
-                "{ \"builds\": [] }",
-                nullArtifactsPayload(),
-                emptyArtifactsPayload(),
-                incompleteArtifactsPayload()
-        );
+        static List<String> incompletePayloads() {
+            return List.of(
+                    "{}",
+                    "{ \"builds\": [] }",
+                    nullArtifactsPayload(),
+                    emptyArtifactsPayload(),
+                    incompleteArtifactsPayload()
+            );
+        }
     }
 }
