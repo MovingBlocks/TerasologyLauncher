11ea71a0839251f138c9d2a57f125fa363222cea
Tobias Nett <skaldarnar@googlemail.com>
2021-02-21 11:30:48 +0100

    doc: add Javadoc for ReleaseMetadata

--- src/main/java/org/terasology/launcher/model/ReleaseMetadata.java
+++ src/main/java/org/terasology/launcher/model/ReleaseMetadata.java
@@ -7,7 +7,10 @@ import java.util.Date;
 import java.util.List;
 
 /**
+ * Data container for metadata associated with a game release.
  *
+ * The metadata in this class is either relevant for displaying more information to the user (e.g., {@code changelog},
+ * {@code timestamp}) or for managing and starting the game itself (e.g., {@code isLwjgl3}).
  */
 public class ReleaseMetadata {
     final List<String> changelog;
@@ -20,11 +23,24 @@ public class ReleaseMetadata {
         this.isLwjgl3 = isLwjgl3;
     }
 
+    /**
+     * The change log of this release, each line represents one logical change.
+     */
     public List<String> getChangelog() {
         return changelog;
     }
 
+    /**
+     * The timestamp of the CI run that built this release.
+     */
     public Date getTimestamp() {
         return timestamp;
+    }
+
+    /**
+     * Whether this release uses LWJGL v3 or not.
+     */
+    public boolean isLwjgl3() {
+        return isLwjgl3;
     }
 }
