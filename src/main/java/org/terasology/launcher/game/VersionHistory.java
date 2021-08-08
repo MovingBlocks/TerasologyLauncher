e92da9d24ec63ed54515acf0d6b3d4a60384ec6b
Kevin Turner <83819+keturn@users.noreply.github.com>
2021-08-08 08:42:46 -0700

    fix(VersionHistory): picocli begins with 5.2.0-SNAPSHOT
    Changed the comparison mechanism from a `Requirement` to `Semver#isGreaterThanOrEqualTo`
    because Requirement doesn't parse pre-release versions.

--- src/main/java/org/terasology/launcher/game/VersionHistory.java
+++ src/main/java/org/terasology/launcher/game/VersionHistory.java
@@ -3,7 +3,6 @@
 
 package org.terasology.launcher.game;
 
-import com.vdurmont.semver4j.Requirement;
 import com.vdurmont.semver4j.Semver;
 import org.terasology.launcher.model.GameIdentifier;
 
@@ -17,17 +16,17 @@ public enum VersionHistory {
      * The preview release of v4.1.0-rc.1 is the first release with LWJGL v3.
      * See https://github.com/MovingBlocks/Terasology/releases/tag/v4.1.0-rc.1
      */
-    LWJGL3("[4.1.0,)"),
-    PICOCLI("[5.1.0,)");
+    LWJGL3("4.1.0-rc.1"),
+    PICOCLI("5.2.0-SNAPSHOT");
 
-    public final Requirement engineVersion;
+    public final Semver engineVersion;
 
     VersionHistory(String s) {
-        engineVersion = Requirement.buildIvy(s);
+        engineVersion = new Semver(s, Semver.SemverType.IVY);
     }
 
     boolean isProvidedBy(Semver version) {
-        return engineVersion.isSatisfiedBy(version);
+        return version.isGreaterThanOrEqualTo(engineVersion);
     }
 
     boolean isProvidedBy(GameIdentifier version) {
