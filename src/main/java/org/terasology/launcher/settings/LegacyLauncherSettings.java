cbbbc7c32b040d6332b3db8c230f3ef29b05c64f
Tobias Nett <skaldarnar@gmail.com>
2021-08-31 22:23:30 +0200

    chore: starting clean-up of LegacyLauncherSettings

--- src/main/java/org/terasology/launcher/settings/LegacyLauncherSettings.java
+++ src/main/java/org/terasology/launcher/settings/LegacyLauncherSettings.java
@@ -55,26 +55,14 @@ public class LegacyLauncherSettings {
     static final JavaHeapSize MAX_HEAP_SIZE_DEFAULT = JavaHeapSize.NOT_USED;
     static final JavaHeapSize INITIAL_HEAP_SIZE_DEFAULT = JavaHeapSize.NOT_USED;
     static final boolean CLOSE_LAUNCHER_AFTER_GAME_START_DEFAULT = true;
-    static final boolean SAVE_DOWNLOADED_FILES_DEFAULT = false;
-    static final boolean SHOW_PRE_RELEASES_DEFAULT = false;
-    static final String LAST_PLAYED_GAME_VERSION_DEFAULT = "";
-    static final String LAST_INSTALLED_GAME_VERSION_DEFAULT = "";
-
     static final String LAUNCHER_SETTINGS_FILE_NAME = "TerasologyLauncherSettings.properties";
 
     private static final String WARN_MSG_INVALID_VALUE = "Invalid value '{}' for the parameter '{}'!";
-    private static final Level LOG_LEVEL_DEFAULT = Level.INFO;
 
     private final Properties properties;
 
-    private final Property<Boolean> showPreReleases = new SimpleBooleanProperty(SHOW_PRE_RELEASES_DEFAULT);
-
     LegacyLauncherSettings(Properties properties) {
         this.properties = properties;
-    }
-
-    Properties getProperties() {
-        return properties;
     }
 
     // --------------------------------------------------------------------- //
@@ -83,172 +71,6 @@ public class LegacyLauncherSettings {
 
     public synchronized void init() {
         logger.trace("Init launcher settings ...");
-
-        initLocale();
-        initMaxHeapSize();
-        initInitialHeapSize();
-        initBaseJavaParameters();
-        initCloseLauncherAfterGameStart();
-        initSaveDownloadedFiles();
-        initShowPreReleases();
-        initGameDirectory();
-        initGameDataDirectory();
-        initUserJavaParameters();
-        initUserGameParameters();
-        initLogLevel();
-        initLastPlayedGameVersion();
-        initLastInstalledGameVersion();
-    }
-
-    void initLocale() {
-        final String localeStr = properties.getProperty(PROPERTY_LOCALE);
-        if (localeStr != null) {
-            Languages.init(localeStr);
-
-            if (!Languages.getCurrentLocale().toString().equals(localeStr)) {
-                logger.warn(WARN_MSG_INVALID_VALUE, localeStr, PROPERTY_LOCALE);
-            }
-        }
-        properties.setProperty(PROPERTY_LOCALE, Languages.getCurrentLocale().toString());
-    }
-
-    void initMaxHeapSize() {
-        final String maxHeapSizeStr = properties.getProperty(PROPERTY_MAX_HEAP_SIZE);
-        JavaHeapSize maxJavaHeapSize = MAX_HEAP_SIZE_DEFAULT;
-        if (maxHeapSizeStr != null) {
-            try {
-                maxJavaHeapSize = JavaHeapSize.valueOf(maxHeapSizeStr);
-            } catch (IllegalArgumentException e) {
-                logger.warn(WARN_MSG_INVALID_VALUE, maxHeapSizeStr, PROPERTY_MAX_HEAP_SIZE);
-            }
-        }
-        properties.setProperty(PROPERTY_MAX_HEAP_SIZE, maxJavaHeapSize.name());
-    }
-
-    void initInitialHeapSize() {
-        final String initialHeapSizeStr = properties.getProperty(PROPERTY_INITIAL_HEAP_SIZE);
-        JavaHeapSize initialJavaHeapSize = INITIAL_HEAP_SIZE_DEFAULT;
-        if (initialHeapSizeStr != null) {
-            try {
-                initialJavaHeapSize = JavaHeapSize.valueOf(initialHeapSizeStr);
-            } catch (IllegalArgumentException e) {
-                logger.warn(WARN_MSG_INVALID_VALUE, initialHeapSizeStr, PROPERTY_INITIAL_HEAP_SIZE);
-            }
-        }
-        properties.setProperty(PROPERTY_INITIAL_HEAP_SIZE, initialJavaHeapSize.name());
-    }
-
-    void initBaseJavaParameters() {
-    }
-
-    void initUserJavaParameters() {
-        final String userJavaParsStr = properties.getProperty(PROPERTY_USER_JAVA_PARAMETERS);
-        if (userJavaParsStr == null || userJavaParsStr.isEmpty()) {
-            properties.setProperty(PROPERTY_USER_JAVA_PARAMETERS, USER_JAVA_PARAMETERS_DEFAULT);
-        }
-    }
-
-    void initUserGameParameters() {
-        final String userJavaParsStr = properties.getProperty(PROPERTY_USER_GAME_PARAMETERS);
-        if (userJavaParsStr == null || userJavaParsStr.isEmpty()) {
-            properties.setProperty(PROPERTY_USER_GAME_PARAMETERS, USER_GAME_PARAMETERS_DEFAULT);
-        }
-    }
-
-    void initLogLevel() {
-        final String logLevelStr = properties.getProperty(PROPERTY_LOG_LEVEL);
-        Level logLevel = LOG_LEVEL_DEFAULT;
-        if (logLevelStr != null) {
-            try {
-                logLevel = Level.valueOf(logLevelStr);
-            } catch (IllegalArgumentException e) {
-                logger.warn(WARN_MSG_INVALID_VALUE, logLevelStr, PROPERTY_LOG_LEVEL);
-            }
-        }
-        properties.setProperty(PROPERTY_LOG_LEVEL, logLevel.name());
-    }
-
-    void initCloseLauncherAfterGameStart() {
-        final String closeLauncherAfterGameStartStr = properties.getProperty(PROPERTY_CLOSE_LAUNCHER_AFTER_GAME_START);
-        boolean closeLauncherAfterGameStart = CLOSE_LAUNCHER_AFTER_GAME_START_DEFAULT;
-        if (closeLauncherAfterGameStartStr != null) {
-            closeLauncherAfterGameStart = Boolean.parseBoolean(closeLauncherAfterGameStartStr);
-        }
-        properties.setProperty(PROPERTY_CLOSE_LAUNCHER_AFTER_GAME_START, Boolean.toString(closeLauncherAfterGameStart));
-    }
-
-    void initSaveDownloadedFiles() {
-        final String saveDownloadedFilesStr = properties.getProperty(PROPERTY_SAVE_DOWNLOADED_FILES);
-        boolean saveDownloadedFiles = SAVE_DOWNLOADED_FILES_DEFAULT;
-        if (saveDownloadedFilesStr != null) {
-            saveDownloadedFiles = Boolean.parseBoolean(saveDownloadedFilesStr);
-        }
-        properties.setProperty(PROPERTY_SAVE_DOWNLOADED_FILES, Boolean.toString(saveDownloadedFiles));
-    }
-
-    void initShowPreReleases() {
-        final String showPreReleasesStr = properties.getProperty(PROPERTY_SHOW_PRE_RELEASES);
-        if (showPreReleasesStr != null) {
-            setShowPreReleases(Boolean.parseBoolean(showPreReleasesStr));
-        } else {
-            setShowPreReleases(SHOW_PRE_RELEASES_DEFAULT);
-        }
-    }
-
-    void initGameDirectory() {
-        final String gameDirectoryStr = properties.getProperty(PROPERTY_GAME_DIRECTORY);
-        Path gameDirectory = null;
-        if (gameDirectoryStr != null && gameDirectoryStr.trim().length() > 0) {
-            try {
-                gameDirectory = Paths.get(new URI(gameDirectoryStr));
-            } catch (URISyntaxException | RuntimeException e) {
-                logger.warn(WARN_MSG_INVALID_VALUE, gameDirectoryStr, PROPERTY_GAME_DIRECTORY);
-            }
-        }
-        if (gameDirectory != null) {
-            properties.setProperty(PROPERTY_GAME_DIRECTORY, gameDirectory.toUri().toString());
-        } else {
-            properties.setProperty(PROPERTY_GAME_DIRECTORY, "");
-        }
-    }
-
-    void initGameDataDirectory() {
-        final String gameDataDirectoryStr = properties.getProperty(PROPERTY_GAME_DATA_DIRECTORY);
-        Path gameDataDirectory = null;
-        if (gameDataDirectoryStr != null && gameDataDirectoryStr.trim().length() > 0) {
-            try {
-                gameDataDirectory = Paths.get(new URI(gameDataDirectoryStr));
-            } catch (URISyntaxException | RuntimeException e) {
-                logger.warn(WARN_MSG_INVALID_VALUE, gameDataDirectoryStr, PROPERTY_GAME_DATA_DIRECTORY);
-            }
-        }
-        if (gameDataDirectory != null) {
-            properties.setProperty(PROPERTY_GAME_DATA_DIRECTORY, gameDataDirectory.toUri().toString());
-        } else {
-            properties.setProperty(PROPERTY_GAME_DATA_DIRECTORY, "");
-        }
-    }
-
-    void initLastPlayedGameVersion() {
-        final String lastPlayedGameVersionStr = properties.getProperty(PROPERTY_LAST_PLAYED_GAME_VERSION);
-        if (lastPlayedGameVersionStr == null || lastPlayedGameVersionStr.isEmpty()) {
-            properties.setProperty(PROPERTY_LAST_PLAYED_GAME_VERSION, LAST_PLAYED_GAME_VERSION_DEFAULT);
-        }
-    }
-
-    void initLastInstalledGameVersion() {
-        final String lastInstalledGameVersionStr = properties.getProperty(PROPERTY_LAST_INSTALLED_GAME_VERSION);
-        if (lastInstalledGameVersionStr == null || lastInstalledGameVersionStr.isEmpty()) {
-            properties.setProperty(PROPERTY_LAST_INSTALLED_GAME_VERSION, LAST_INSTALLED_GAME_VERSION_DEFAULT);
-        }
-    }
-
-    // --------------------------------------------------------------------- //
-    // PROPERTIES
-    // --------------------------------------------------------------------- //
-
-    public ReadOnlyProperty<Boolean> showPreReleases() {
-        return showPreReleases;
     }
 
     // --------------------------------------------------------------------- //
@@ -389,11 +211,6 @@ public class LegacyLauncherSettings {
 
     public synchronized void setKeepDownloadedFiles(boolean keepDownloadedFiles) {
         properties.setProperty(PROPERTY_SAVE_DOWNLOADED_FILES, Boolean.toString(keepDownloadedFiles));
-    }
-
-    public synchronized void setShowPreReleases(boolean selected) {
-        showPreReleases.setValue(selected);
-        properties.setProperty(PROPERTY_SHOW_PRE_RELEASES, Boolean.toString(selected));
     }
 
     public synchronized void setGameDirectory(Path gameDirectory) {
