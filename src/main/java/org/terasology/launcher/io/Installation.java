6c2c31a5e203a61f6b740a6dff85bfbccb781241
Tobias Nett <skaldarnar@googlemail.com>
2023-11-25 15:53:08 +0100

    chore: add `Installation<T>` interface
    Intended use is to describe installations of JREs (`Installation<Jre>`) and Games (`Installation<Game>`) alike

---
+++ src/main/java/org/terasology/launcher/io/Installation.java
@@ -0,0 +1,19 @@
+// Copyright 2023 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+package org.terasology.launcher.io;
+
+import java.nio.file.Path;
+
+public interface Installation<T> {
+    /**
+     * @return The full path to the location of the installation.
+     */
+    Path getPath();
+
+    /**
+     * @return The information object describing <i>what</i> is installed.
+     */
+    T getInfo();
+
+}
